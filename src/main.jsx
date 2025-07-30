c1db3e0e85bbd3f5c0c1260c5b2ed0d7e3d2af75
Cristian Yulma <cristianyulmaw@gmail.com>
2025-07-30 07:45:13 +0700

    Feat: Main file for calling all react file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import '../src/style/index.css'
+// import './style/Login.css'
+import './style/home2.css'
+import Footer from './components/Footer.jsx'
+import Register from './components/Register.jsx';
+import Login from './components/Login.jsx';
+import Movies from './components/Movies.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <MyCounter /> */}
+    {/* <Tabel /> */}
+    {/* <Register /> */}
+    <Movies />
+    {/* <Login /> */}
+    {/* <DomMinitask /> */}
   </StrictMode>,
 )
